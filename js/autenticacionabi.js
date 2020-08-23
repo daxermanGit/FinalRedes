68d69bc796c560e2f21695a277f584e6fee21fa1
LCalderon <calde2010@hotmail.es>
2020-08-23 11:29:38 -0500

    first commit

---
+++ js/autenticacionabi.js
@@ -0,0 +1,170 @@
+var AutenticacionABI = [
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "usuarios",
+		"outputs": [
+			{
+				"name": "nombre",
+				"type": "string"
+			},
+			{
+				"name": "apellido",
+				"type": "string"
+			},
+			{
+				"name": "identificacion",
+				"type": "string"
+			},
+			{
+				"name": "email",
+				"type": "string"
+			},
+			{
+				"name": "usuario",
+				"type": "string"
+			},
+			{
+				"name": "contrasena",
+				"type": "uint256"
+			},
+			{
+				"name": "edad",
+				"type": "uint256"
+			},
+			{
+				"name": "clave",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_nombre",
+				"type": "string"
+			},
+			{
+				"name": "_apellido",
+				"type": "string"
+			},
+			{
+				"name": "_identificacion",
+				"type": "string"
+			},
+			{
+				"name": "_email",
+				"type": "string"
+			},
+			{
+				"name": "_usuario",
+				"type": "string"
+			},
+			{
+				"name": "_contrasena",
+				"type": "string"
+			},
+			{
+				"name": "_edad",
+				"type": "uint256"
+			}
+		],
+		"name": "registrarse",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "a",
+				"type": "string"
+			},
+			{
+				"name": "b",
+				"type": "string"
+			}
+		],
+		"name": "compararStrings",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_usuario",
+				"type": "string"
+			},
+			{
+				"name": "_contrasena",
+				"type": "string"
+			}
+		],
+		"name": "iniciarSesion",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_nombre",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_apellido",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_identificacion",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_email",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_usuario",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_contrasena",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "_edad",
+				"type": "uint256"
+			}
+		],
+		"name": "NewUser",
+		"type": "event"
+	}
+]
